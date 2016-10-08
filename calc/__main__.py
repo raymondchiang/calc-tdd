49f1575d2329aa08918c261cc5fdbbb446968d6f
raymondchiang <chiangnarry@gmail.com>
2016-10-08 21:58:16 +0800

    revise

--- calc/__main__.py
+++ calc/__main__.py
@@ -1,25 +1,27 @@
 from colorama import Fore, init
 init(autoreset=True)
-
-#-------------------------------
-def conv_type(number):
-    if '.' in number:
-        return float(number)
-    return int(number)
 #-------------------------------
 def str_to_int(lis):
     for x in range(len(lis)):
         lis[x]=conv_type(lis[x])
     return lis
+
+def conv_type(number):
+    if '.' in number:
+        return float(number)
+    return int(number)
 #-------------------------------
 def split_str(stri):
     number=[]
     symbol=[]
     curr_num=[]
-    if stri[0]=='-':
-        stri='0'+stri
     for x in range(len(stri)):
-        if stri[x]=='+' or stri[x]=='-' or stri[x]=='*' or stri[x]=='/' :
+        if stri[x]=='-':
+            if len(curr_num):
+                symbol+='+'
+                number+=[''.join(curr_num),]
+            curr_num=['-']
+        elif stri[x] in ['+','*','/']:
             symbol+=stri[x]
             number+=[''.join(curr_num),]
             curr_num=[]
@@ -27,7 +29,7 @@ def split_str(stri):
             curr_num+=stri[x]
         if x==len(stri)-1:
             number+=[''.join(curr_num),]
-    return (number,symbol)
+    return number, symbol
 #-------------------------------
 def muti_divi(arr,sym):
     sym+=['end',]
@@ -50,24 +52,13 @@ def muti_divi(arr,sym):
     del(sym[len(sym)-1])
     return (arr,sym)
 #-------------------------------
-def add_minus(num,sym):
-    ans=num[0]
-    for x in range(len(sym)):
-        if sym[x]=='+':
-            ans+=num[x+1]
-        else:
-            ans-=num[x+1]
-    return ans
-#-------------------------------
 def calc(statement):
     symbol=[]
     num=[]
-    ans=0
     num,symbol=split_str(statement)
     num=str_to_int(num)
     num,symbol=muti_divi(num,symbol)
-    ans=add_minus(num,symbol)
-    return (ans)
+    return sum(num)
 #-------------------------------
 if __name__ == '__main__':
     while True:
