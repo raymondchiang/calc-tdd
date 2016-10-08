c98d4fa23d3379e1efc442b832681e372c65a662
raymondchiang <chiangnarry@gmail.com>
2016-10-08 17:08:41 +0800

    Add multi

--- calc/__main__.py
+++ calc/__main__.py
@@ -1,42 +1,71 @@
 #-------------------------------
-def conv_sym(num):
-    if '.' in num:
-        return float(num)
-    return int(num)
+def conv_type(number):
+    if '.' in number:
+        return float(number)
+    return int(number)
 #-------------------------------
-def conv_str(lis):
+def str_to_int(lis):
     for x in range(len(lis)):
-        lis[x]=conv_sym(lis[x])
+        lis[x]=conv_type(lis[x])
     return lis
 #-------------------------------
-def calc(statement):
-    add='+'
-    minus='-'
+def split_str(stri):
+    number=[]
     symbol=[]
-    num=[]
     curr_num=[]
-    ans=0
-    if statement[0]==minus:
-        statement='0'+statement
-    for x in range(len(statement)):
-        if statement[x]==add or statement[x]==minus:
-            symbol+=statement[x]
-            num+=[''.join(curr_num),]
+    if stri[0]=='-':
+        stri='0'+stri
+    for x in range(len(stri)):
+        if stri[x]=='+' or stri[x]=='-' or stri[x]=='*' or stri[x]=='/' :
+            symbol+=stri[x]
+            number+=[''.join(curr_num),]
             curr_num=[]
         else:
-            curr_num+=statement[x]
-        if x==len(statement)-1:
-            num+=[''.join(curr_num),]
-
-    num=conv_str(num)
+            curr_num+=stri[x]
+        if x==len(stri)-1:
+            number+=[''.join(curr_num),]
+    return (number,symbol)
+#-------------------------------
+def muti_divi(arr,sym):
+    sym+=['end',]
+    length=len(sym)
+    s=0
+    while s<length:
+        if sym[s]=='end':
+            break
+        if sym[s]=='*':
+            arr[s]*=arr[s+1]
+            del(arr[s+1])
+            del(sym[s])
+            continue
+        if sym[s]=='/':
+            arr[s]/=arr[s+1]
+            del(arr[s+1])
+            del(sym[s])
+            continue
+        s+=1
+    del(sym[len(sym)-1])
+    return (arr,sym)
+#-------------------------------
+def add_minus(num,sym):
     ans=num[0]
-    for x in range(len(num)-1):
-        if symbol[x]==add:
+    for x in range(len(sym)):
+        if sym[x]=='+':
             ans+=num[x+1]
         else:
             ans-=num[x+1]
     return ans
-
+#-------------------------------
+def calc(statement):
+    symbol=[]
+    num=[]
+    ans=0
+    num,symbol=split_str(statement)
+    num=str_to_int(num)
+    num,symbol=muti_divi(num,symbol)
+    ans=add_minus(num,symbol)
+    return (ans)
+#-------------------------------
 if __name__ == '__main__':
     while True:
         print(calc(input('Input a statement:')))
